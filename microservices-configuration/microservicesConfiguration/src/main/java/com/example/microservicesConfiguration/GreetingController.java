b1bdc4de7bc26ebd8442799a5a1371d101f5bdf8
Jayant Rai <jayant.chamling@outlook.com>
2020-05-22 17:34:06 -0500

    @Value method of configuration

--- microservices-configuration/microservicesConfiguration/src/main/java/com/example/microservicesConfiguration/GreetingController.java
+++ microservices-configuration/microservicesConfiguration/src/main/java/com/example/microservicesConfiguration/GreetingController.java
@@ -1,5 +1,8 @@
 package com.example.microservicesConfiguration;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 	
 	// accessing property file using @Value annotation to display the value
-	@Value("${my.greeting}")
+	// if there is no value in the properties file, we can set default value using colon
+	// the application will not fail 
+	@Value("${ my.greeting: default value }")
 	private String greetingMessage;
+	
+	// another method 
+	@Value("some static message")
+	private String staticMessage;
+	
+	
+	// if we want a list of values
+	@Value("${my.list.values}")
+	private List<String> listValues;
+	
+	// # to evalute it as an expression
+	@Value("#{${dbValue}}")
+	private Map<String, String> dbValues;
 	
 	@GetMapping("/greeting")
 	public String greeting() {
-		return greetingMessage;
+		return greetingMessage + staticMessage + listValues + dbValues;
 	}
 }
