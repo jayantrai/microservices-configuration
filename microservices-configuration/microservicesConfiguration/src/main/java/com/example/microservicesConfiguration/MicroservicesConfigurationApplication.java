b59b10412c99f18c9ad891f919a82d2d7fb2ec73
Jayant Rai <jayant.chamling@outlook.com>
2020-05-22 15:17:59 -0500

    project setup

---
+++ microservices-configuration/microservicesConfiguration/src/main/java/com/example/microservicesConfiguration/MicroservicesConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicesConfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesConfigurationApplication.class, args);
+	}
+
+}
