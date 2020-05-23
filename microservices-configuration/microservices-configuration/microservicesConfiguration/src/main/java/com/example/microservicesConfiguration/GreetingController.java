5c27c119264fcd9575b4937738220b9b45a39214
Jayant Rai <jayant.chamling@outlook.com>
2020-05-23 01:33:22 -0500

    environment object but not recommended

--- microservices-configuration/microservices-configuration/microservicesConfiguration/src/main/java/com/example/microservicesConfiguration/GreetingController.java
+++ microservices-configuration/microservices-configuration/microservicesConfiguration/src/main/java/com/example/microservicesConfiguration/GreetingController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,9 +34,16 @@ public class GreetingController {
 	@Autowired
 	private DbSettings dbSettings;
 	
+//	@Autowired
+//	private Environment env;
 	
 	@GetMapping("/greeting")
 	public String greeting() {
 		return dbSettings.getConnection() + dbSettings.getHost() + dbSettings.getPort();
 	}
+	
+//	@GetMapping("/envdetails")
+//	public String[] envDetails() {
+//		return env.getActiveProfiles();
+//	}
 }
